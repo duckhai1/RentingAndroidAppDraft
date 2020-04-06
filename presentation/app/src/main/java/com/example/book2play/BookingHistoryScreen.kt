979ba0929fa089e74897c4539cb63acfcd513c34
duckhai1 <huynhduc.khai0@gmail.com>
2020-04-06 15:42:48 +0700

    change screen name for globally project

--- presentation/app/src/main/java/com/example/book2play/BookingHistoryScreen.kt
+++ presentation/app/src/main/java/com/example/book2play/BookingHistoryScreen.kt
@@ -2,13 +2,13 @@ package com.example.book2play
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.booking_history_screen.*
 
-class MainActivity : AppCompatActivity() {
+class BookingHistoryScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.booking_history_screen)
         toolBar.setTitle("Booking history")
         setSupportActionBar(toolBar)
 
