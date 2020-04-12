54619f495a7be5dc63260a5c2ac62aed81076cfa
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2020-04-12 19:28:31 +0700

    New Get procedures

---
+++ database/book2play_model/stored_procedures/getBookingsInCourt.sql
@@ -0,0 +1,59 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS getBookingsInCourt//
+CREATE PROCEDURE getBookingsInCourt(
+	IN inCourtId VARCHAR(100),
+	IN inCityId VARCHAR(100),
+	IN inSportcenterId VARCHAR(100),
+    OUT statusCode INT
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		GET STACKED DIAGNOSTICS CONDITION 1 @p1 = MYSQL_ERRNO;
+		SET statusCode = @p1;
+		ROLLBACK;
+	END;
+
+	START TRANSACTION;
+    
+    IF inCityId NOT IN (SELECT cityId FROM cities) THEN
+		SIGNAL SQLSTATE '45000'
+			SET MYSQL_ERRNO = 460; -- invalid city id
+	END IF;
+    
+	IF inSportcenterId NOT IN (
+		SELECT sportcenterId 
+		FROM sportcenters
+		NATURAL JOIN cities
+		WHERE cityId = inCityId
+	) THEN
+		SIGNAL SQLSTATE '45000'
+			SET MYSQL_ERRNO = 461; -- sportcenter id does not exist
+	END IF;
+    
+    IF inCourtId NOT IN (
+		SELECT courtId
+		FROM courts
+		NATURAL JOIN sportcenters
+		NATURAL JOIN cities
+		WHERE cityId = inCityId
+			AND sportcenterId = inSportcenterId
+	) THEN
+		SIGNAL SQLSTATE '45000'
+			SET MYSQL_ERRNO = 462; -- invalid court id
+	END IF;
+    
+    SET statusCode = 200;
+    
+    SELECT bookingId, courtId, sportcenterId, cityId, bookingStartTime, bookingEndTime
+	FROM bookings
+	NATURAL JOIN courts
+	NATURAL JOIN sportcenters
+	NATURAL JOIN cities
+	WHERE cityId = inCityId
+		AND sportcenterId = inSportcenterId
+		AND courtId = inCourtId;
+END//
+
+DELIMITER ;
