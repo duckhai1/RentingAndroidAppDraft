77956a4549a3fa8d09de9783b5bf528e4589cf83
Hoang Xuan Bac <62278951+bacxhoang@users.noreply.github.com>
2020-04-22 01:07:16 +0700

    added new getPlayerBookings, changed old getPlayerBookings -> getPlayerBookingsInCity

--- database/book2play_model/stored_procedures/getPlayerBookings.sql
+++ database/book2play_model/stored_procedures/getPlayerBookings.sql
@@ -1,43 +1,26 @@
 /*
-	Get all bookings of a player in the given city and date
+	Get all bookings of a player with the given player Id
 */
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS getPlayerBookings //
 CREATE PROCEDURE getPlayerBookings (
 	IN inPlayerId VARCHAR(100),
-	IN inCityId VARCHAR(100),
-	IN inBookingDate DATE,
     OUT statusCode INT
 )
 BEGIN
-	IF inCityId NOT IN (SELECT cityId FROM cities) THEN
-		SET statusCode = 460; -- city id does not exist
-	ELSEIF inPlayerId NOT IN (SELECT playerId FROM players) THEN
-		SET statusCode = 464;
-	ELSEIF NOT EXISTS (
-		SELECT *
-		FROM bookings
-		NATURAL JOIN courts
-		NATURAL JOIN sportCenters
-		NATURAL JOIN cities
-		NATURAL JOIN players
-		WHERE cityId = inCityID
-			AND playerId = inPlayerId
-			AND bookingDate = inBookingDate
-	) THEN
-		SET statusCode = 466; -- no bookings in given date 
+
+	IF inPlayerId NOT IN (SELECT playerId FROM players) THEN
+		SET statusCode = 464; invalid player id
     ELSE
 		SET statusCode = 200;
-		SELECT bookingId, createdAt, bookingDate, bookingStartTime, bookingEndTime, isPaid, cityId, sportCenterId, courtId, player
+		SELECT bookingId, createdAt, bookingDate, bookingStartTime, bookingEndTime, isPaid, cityId, sportCenterId, courtId, playerId
 		FROM bookings
 			NATURAL JOIN players
 			NATURAL JOIN courts
 			NATURAL JOIN sportCenters
 			NATURAL JOIN cities
-		WHERE cityId = inCityId
-			AND bookingDate = inBookingDate
-			AND playerId = inPlayerId;
+		WHERE playerId = inPlayerId;
 	END IF;
 END//
 
