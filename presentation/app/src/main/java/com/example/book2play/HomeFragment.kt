187405bd63b6dd5dbd952d3e0b5b4c5921a112e2
Huỳnh Đức Khải <43449109+duckhai1@users.noreply.github.com>
2020-04-22 19:19:01 +0700

    Presentation/raw app v3.1 (#40)
    * Create Home Fragment
    
    * create padding for home fragment
    
    Co-authored-by: Hieu Le <13507@student.vgu.edu.vn>

--- presentation/app/src/main/java/com/example/book2play/HomeFragment.kt
+++ presentation/app/src/main/java/com/example/book2play/HomeFragment.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.book2play.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +19,21 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val arrayList = ArrayList<CourtModel>()
+        arrayList.add(CourtModel("BaoDoi Sporting Center", "145 Le Duan Street",  "Binh Duong city",5.0,R.drawable.sancaulong,"20% off for 1 month","40000 VND","50000 VND"))
+        arrayList.add(CourtModel("HieuLon Sporting Center", "199 Phan Xich Long Street",  "Vung Tau city",4.5,R.drawable.sancaulong,"20% off for 1 month","40000 VND","50000 VND"))
+        arrayList.add(CourtModel("PhatCho Sporting Center", "59 Dien Bien Phu Street",  "My Tho city",3.5,R.drawable.sancaulong,"20% off for 1 month","40000 VND","50000 VND"))
+
+        val myAdapter = HomeAdapter(arrayList,this)
+        recyclerView4.layoutManager= LinearLayoutManager(activity)
+        recyclerView4.adapter = myAdapter
+
+
+
     }
 
 }
