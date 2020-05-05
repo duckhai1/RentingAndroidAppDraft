814b8113bcdfea660953be81d7ff6eef253bc32d
duckhai1 <huynhduc.khai0@gmail.com>
2020-05-05 11:44:10 +0700

    fix error code 405

--- presentation/app/src/main/java/com/example/LogicConnection/AsyncClass/CreatePlayerAsync.kt
+++ presentation/app/src/main/java/com/example/LogicConnection/AsyncClass/CreatePlayerAsync.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 import com.example.LogicConnection.Handler.ConnectionHandler
 import com.example.book2play.ScreenView.Activity.LoginScreen
 import java.lang.Exception
@@ -23,6 +24,36 @@ class CreatePlayerAsync(activity: Activity) : MyGeneralAsyncTask(activity) {
             Log.d("server_connect",  "Exception: " + e.message)
         }
         return result
+    }
+
+    override fun onPostExecute(result: String?) {
+        if (dialog.isShowing) {
+            dialog.dismiss();
+        }
+
+        // if player already booked
+        if (Integer.parseInt(result.toString()) == 405){
+            Toast.makeText(activity, "Welcome back book2player", Toast.LENGTH_SHORT).show()
+        }
+        // Bad request to server
+        if (Integer.parseInt(result.toString()) in 400..500){
+            jobUnrecognized()
+        }
+        // if connect successful
+        else if (error == null) {
+            jobSuccess()
+        }
+        // can not connect to server
+        else {
+            val builder = AlertDialog.Builder(activity)
+            builder.setTitle("Connection error")
+            builder.setMessage("Can not connect to the server. Please check you internet connection")
+            builder.setPositiveButton("OK"){dialog, which ->
+                jobFail()
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
     }
 
     override fun jobFail() {
